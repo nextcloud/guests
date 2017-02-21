a20f439d1de8e1089ceea9ef3eac3f8962a6a38e
Ilja Neumann <ineumann@owncloud.com>
2017-02-21 13:11:38 +0100

    Fixed licence of js files, which were ignored by licence tool

--- js/guests.js
+++ js/guests.js
@@ -2,13 +2,19 @@
  * ownCloud
  *
  * @author Jörn Friedrich Dreyer <jfd@owncloud.com>
- * @copyright (C) 2015-2016 ownCloud, Inc.
+ * @copyright (C) 2015-2017 ownCloud, Inc.
  *
- * This code is covered by the ownCloud Commercial License.
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
  *
- * You should have received a copy of the ownCloud Commercial License
- * along with this program. If not, see <https://owncloud.com/licenses/owncloud-commercial/>.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
  *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 (function() {
 
