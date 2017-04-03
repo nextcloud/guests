a4821d32df277d4f8a4e6f64b6846509da09724a
Sergio Bertolin <sbertolin@solidgear.es>
2017-04-03 13:13:32 +0000

    Adding first features for creating tests

--- tests/integration/features/bootstrap/GuestsContext.php
+++ tests/integration/features/bootstrap/GuestsContext.php
@@ -21,18 +21,98 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use GuzzleHttp\Client;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
 
 /**
- * Guest context.
+ * Guests context.
  */
-class GuestContext implements Context, SnippetAcceptingContext {
+class GuestsContext implements Context, SnippetAcceptingContext {
 	use Webdav;
 
 	/** @var array */
 	private $createdGuests = [];
+
+	/**
+	 * @Given user :user creates guest user :guestDisplayName with email :guestEmail
+	 * @param string $user
+	 * @param string $guestDisplayName
+	 * @param string $guestEmail
+	 */
+	public function userCreatedAGuestUser($user, $guestDisplayName, $guestEmail) {
+		$fullUrl = substr($this->baseUrl, 0, -4) . '/index.php/apps/guests/users';
+		//Replicating frontend behaviour
+		$emailDomain = preg_split('/\./', preg_split('/@/', $guestEmail, null, null)[1], null, null);
+		$userName = $guestDisplayName . '_' . $emailDomain[0] . '_' . $emailDomain[1];
+		$fullUrl = $fullUrl . '?displayName=' . $guestDisplayName . '&email=' . $guestEmail . '&username=' . $userName;
+		$client = new Client();
+		$options = [];
+		if ($user === 'admin') {
+			$options['auth'] = $this->adminUser;
+		} else {
+			$options['auth'] = [$user, $this->regularUser];
+		}
+		$request = $client->createRequest("PUT", $fullUrl, $options);
+		$request->addHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+		try {
+			$this->response = $client->send($request);
+		} catch (\GuzzleHttp\Exception\BadResponseException $e) {
+			// 4xx and 5xx responses cause an exception
+			$this->response = $e->getResponse();
+		}
+		$this->createdGuests[$guestDisplayName] = $guestEmail;
+	}
+
+	public function getGuests($user) {
+		$fullUrl = substr($this->baseUrl, 0, -4) . '/index.php/apps/guests/users';
+
+		$client = new Client();
+
+		$options = [];
+		if ($user === 'admin') {
+			$options['auth'] = $this->adminUser;
+		} else {
+			$options['auth'] = [$user, $this->regularUser];
+		}
+
+		$request = $client->createRequest("GET", $fullUrl, $options);
+
+		try {
+			$this->response = $client->send($request);
+		} catch (\GuzzleHttp\Exception\BadResponseException $e) {
+			// 4xx and 5xx responses cause an exception
+			$this->response = $e->getResponse();
+		}
+
+		return $this->response->xml()->data;
+	}
+
+	/**
+	 * @Then user :user gets guests users they are
+	 * @param string $user
+	 * @param \Behat\Gherkin\Node\TableNode|null $tablenode
+	 */
+	public function getGuestUsers($user, $tablenode) {
+		$guestsList = $this->getGuests($user);
+		print_r($guestsList);
+
+
+		// if ($memberList instanceof \Behat\Gherkin\Node\TableNode) {
+		// 	$members = $memberList->getRows();
+		// 	$membersSimplified = $this->simplifyArray($members);
+		// 	$respondedArray = $this->getCustomGroupMembers($user, $customGroup);
+		// 	foreach ($membersSimplified as $member) {
+		// 		$memberPath = '/' . $this->davPath . $appPath . $customGroup . '/' . $member;
+		// 		if (!array_key_exists($memberPath, $respondedArray)){
+		// 			PHPUnit_Framework_Assert::fail("$member path" . " is not in report answer");
+		// 		}
+		// 	}
+		// }
+	}
+
 
 }
 
--- tests/integration/guests_features/Guests.feature
+++ tests/integration/guests_features/Guests.feature
@@ -3,3 +3,24 @@ Feature: Custom Groups
 Background:
 		Given using api version "1"
 		And using new dav path
+
+Scenario: Creating a guest user works fine
+	Given As an "admin"
+	When user "admin" creates guest user "guest" with email "guest@example.com"
+	Then the HTTP status code should be "201"
+	And user "admin" gets guests users they are
+		| guest | guest@example.com |
+
+Scenario: Check that skeleton is properly set
+	Given As an "admin"
+	And user "user0" exists
+	Then user "user0" should see following elements
+		| /FOLDER/ |
+		| /PARENT/ |
+		| /PARENT/parent.txt |
+		| /textfile0.txt |
+		| /textfile1.txt |
+		| /textfile2.txt |
+		| /textfile3.txt |
+		| /textfile4.txt |
+		| /welcome.txt |
