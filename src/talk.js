9760e04623e19a87e0de52568be8592e05a35dcc
Joas Schilling <coding@schilljs.com>
2022-05-20 14:34:40 +0200

    Add talk integration
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ src/talk.js
@@ -0,0 +1,37 @@
+import { validate } from 'email-validator'
+import Vue from 'vue'
+
+import GuestForm from './views/GuestForm.vue'
+import Nextcloud from './mixins/Nextcloud'
+
+Vue.mixin(Nextcloud)
+
+if (!OCA.Guests) {
+	/**
+	 * @namespace OCA.Guests
+	 */
+	OCA.Guests = {}
+}
+
+// Init guests modal
+const guestForm = new Vue(GuestForm)
+const guestRoot = document.createElement('div')
+guestRoot.setAttribute('id', 'guest-root')
+document.body.appendChild(guestRoot)
+guestForm.$mount('#guest-root')
+
+// await page loading and init ShareSearch
+window.addEventListener('DOMContentLoaded', () => {
+	if (!window.OCA?.Talk?.registerParticipantSearchAction) {
+		return
+	}
+
+	window.OCA.Talk.registerParticipantSearchAction({
+		icon: 'icon-guests',
+		label: t('guests', 'Invite guest'),
+		show: validate,
+		async callback(query) {
+			return guestForm.populate({ app: 'talk' }, query)
+		},
+	})
+})
--- webpack.js
+++ webpack.js
@@ -2,5 +2,6 @@ const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 webpackConfig.entry.settings = path.join(__dirname, 'src', 'settings.js')
+webpackConfig.entry.talk = path.join(__dirname, 'src', 'talk.js')
 
 module.exports = webpackConfig
