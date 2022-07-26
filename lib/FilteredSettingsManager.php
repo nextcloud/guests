fe686bfd4165be3f30bf53d6ed3a370f510ba986
Julius Härtl <jus@bitgrid.net>
2022-07-26 07:48:41 +0200

    Limit settings to whitelisted apps
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ lib/FilteredSettingsManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace OCA\Guests;
+
+use OCP\IUser;
+use OCP\Settings\IManager;
+
+class FilteredSettingsManager implements IManager {
+
+	/** @var IManager */
+	private $manager;
+	/** @var AppWhitelist */
+	private $appWhitelist;
+
+	public function __construct(IManager $manager, AppWhitelist $appWhitelist) {
+		$this->manager = $manager;
+		$this->appWhitelist = $appWhitelist;
+	}
+
+	private function isSettingAllowed(string $setting): bool {
+		$appId = \OC\AppFramework\App::getAppIdForClass($setting);
+		return $this->appWhitelist->isAppWhitelisted($appId);
+	}
+
+	public function registerSection(string $type, string $section) {
+		$this->manager->registerSection($type, $section);
+	}
+
+	public function registerSetting(string $type, string $setting) {
+		if (!$this->isSettingAllowed($setting)) {
+			return;
+		}
+
+		$this->manager->registerSetting($type, $setting);
+	}
+
+	public function getAdminSections(): array {
+		return $this->manager->getAdminSections();
+	}
+
+	public function getPersonalSections(): array {
+		return $this->manager->getPersonalSections();
+	}
+
+	public function getAdminSettings($section, bool $subAdminOnly = false): array {
+		return $this->manager->getAdminSettings($section, $subAdminOnly);
+	}
+
+	public function getAllowedAdminSettings(string $section, IUser $user): array {
+		return $this->manager->getAllowedAdminSettings($section, $user);
+	}
+
+	public function getAllAllowedAdminSettings(IUser $user): array {
+		return $this->manager->getAllAllowedAdminSettings($user);
+	}
+
+	public function getPersonalSettings($section): array {
+		return $this->manager->getPersonalSettings($section);
+	}
+}
