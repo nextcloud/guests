0d432daa7584f4aeeaf40db135beee668abf6189
Christopher Ng <chrng8@gmail.com>
2024-06-11 16:29:20 -0700

    chore: Move to vite
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- webpack.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-const webpackConfig = require('@nextcloud/webpack-vue-config')
-
-webpackConfig.entry.settings = path.join(__dirname, 'src', 'settings.js')
-webpackConfig.entry.talk = path.join(__dirname, 'src', 'talk.js')
-
-module.exports = webpackConfig
