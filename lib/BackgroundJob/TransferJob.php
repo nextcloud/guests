feecf7c371c6d4cae5c5711c25f8fcc5e8769a27
Christopher Ng <chrng8@gmail.com>
2024-07-11 17:12:57 -0700

    feat: Add transfer job
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

---
+++ lib/BackgroundJob/TransferJob.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Guests\BackgroundJob;
+
+use OCA\Guests\AppInfo\Application;
+use OCA\Guests\Db\Transfer;
+use OCA\Guests\Db\TransferMapper;
+use OCA\Guests\GuestManager;
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\BackgroundJob\QueuedJob;
+use OCP\IURLGenerator;
+use OCP\IUser;
+use OCP\IUserManager;
+use OCP\Notification\IManager as NotificationManager;
+use OCP\Security\ISecureRandom;
+use Psr\Log\LoggerInterface;
+
+class TransferJob extends QueuedJob {
+	public function __construct(
+		ITimeFactory $time,
+		private IUserManager $userManager,
+		private ISecureRandom $secureRandom,
+		private NotificationManager $notificationManager,
+		private IURLGenerator $urlGenerator,
+		private GuestManager $guestManager,
+		private TransferMapper $transferMapper,
+		private LoggerInterface $logger,
+	)
+	{
+		parent::__construct($time);
+	}
+
+	private function notifyFailure(Transfer $transfer): void {
+		$notification = $this->notificationManager->createNotification();
+		$notification
+			->setApp(Application::APP_ID)
+			->setIcon($this->urlGenerator->getAbsoluteURL($this->urlGenerator->imagePath(Application::APP_ID, 'account-arrow-right.svg')))
+			->setUser($transfer->getAuthor())
+			->setDateTime($this->time->getDateTime())
+			->setObject('guest-transfer', (string)$transfer->getId())
+			->setSubject('guest-transfer-fail', [
+				'source' => $transfer->getSource(),
+				'target' => $transfer->getTarget(),
+			]);
+		$this->notificationManager->notify($notification);
+	}
+
+	private function notifySuccess(Transfer $transfer): void {
+		$notification = $this->notificationManager->createNotification();
+		$notification
+			->setApp(Application::APP_ID)
+			->setIcon($this->urlGenerator->getAbsoluteURL($this->urlGenerator->imagePath(Application::APP_ID, 'account-arrow-right.svg')))
+			->setUser($transfer->getAuthor())
+			->setDateTime($this->time->getDateTime())
+			->setObject('guest-transfer', (string)$transfer->getId())
+			->setSubject('guest-transfer-done', [
+				'source' => $transfer->getSource(),
+				'target' => $transfer->getTarget(),
+			]);
+		$this->notificationManager->notify($notification);
+	}
+
+	public function run($argument): void {
+		/** @var int $id */
+		$id = $argument['id'];
+
+		$transfer = $this->transferMapper->getById($id);
+		$transfer->setStatus(Transfer::STATUS_STARTED);
+		$this->transferMapper->update($transfer);
+
+		$source = $transfer->getSource();
+		$target = $transfer->getTarget();
+
+		$sourceUser = $this->userManager->get($source);
+		if (!($sourceUser instanceof IUser)) {
+			$this->logger->error('Failed to transfer missing guest user: ' . $source);
+			$this->notifyFailure($transfer);
+			$this->transferMapper->delete($transfer);
+			return;
+		}
+
+		if ($this->userManager->userExists($target)) {
+			$this->logger->error("Cannot transfer guest user \"$source\", target user \"$target\" already exists");
+			$this->notifyFailure($transfer);
+			$this->transferMapper->delete($transfer);
+			return;
+		}
+
+		$targetUser = $this->userManager->createUser(
+			$target,
+			$this->secureRandom->generate(20), // Password hash will be copied to target user from source user
+		);
+
+		if (!($targetUser instanceof IUser)) {
+			$this->logger->error('Failed to create new user: ' . $target);
+			$this->notifyFailure($transfer);
+			$this->transferMapper->delete($transfer);
+			return;
+		}
+
+		$targetUser->setSystemEMailAddress($sourceUser->getUID()); // Guest user id is an email
+
+		// TODO copy password hash to target user
+
+		try {
+			$this->guestManager->transfer($sourceUser, $targetUser);
+			$result = $sourceUser->delete();
+			if (!$result) {
+				$this->logger->error('Failed to delete guest user', ['userId' => $sourceUser->getUID()]);
+			}
+			$this->notifySuccess($transfer);
+		} catch (\Throwable $th) {
+			$this->logger->error($th->getMessage(), ['exception' => $th]);
+			$result = $targetUser->delete(); // Rollback created user
+			if (!$result) {
+				$this->logger->error('Failed to delete target user', ['userId' => $targetUser->getUID()]);
+			}
+			$this->notifyFailure($transfer);
+		}
+		$this->transferMapper->delete($transfer);
+	}
+}
